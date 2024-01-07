058faf4ca41ebdc815a5ce440a23e0d273283b33
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-01-07 14:13:11 -0600

    add apollo client to expo

--- react-expo/App.tsx
+++ react-expo/App.tsx
@@ -5,16 +5,27 @@ import { Provider } from 'react-redux';
 import { store } from './store/rtk-redux/store';
 import { Home } from './store/views/Home';
 
-export default function App() {
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
+// Initialize Apollo Client
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+});
+
+console.log('apollo client', client);
+
+export default function App() {
   return (
-    <Provider store={store}>
-      <View style={styles.container}>
-        <Home />
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style='auto' />
-      </View>
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <Home />
+          <Text>Open up App.tsx to start working on your app!</Text>
+          <StatusBar style='auto' />
+        </View>
+      </Provider>
+    </ApolloProvider>
   );
 }
 
